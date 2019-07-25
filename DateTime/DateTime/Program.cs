ea2d4ecf59f558a16f54015da3861fd703c379e5
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-07-25 21:50:51 +0400

    DateTime.Now.ToString

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+
+            // Format Datetime in different formats and display them  
+            Console.WriteLine(dateTime.ToString("MM/dd/yyyy"));
+            Console.WriteLine(dateTime.ToString("dddd, dd MMMM yyyy"));
+            Console.WriteLine(dateTime.ToString("dddd, dd MMMM yyyy"));
+            Console.WriteLine(dateTime.ToString("dddd, dd MMMM yyyy"));
+            Console.WriteLine(dateTime.ToString("dddd, dd MMMM yyyy"));
+            Console.WriteLine(dateTime.ToString("dddd, dd MMMM yyyy"));
+            Console.WriteLine(dateTime.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+            Console.WriteLine(dateTime.ToString("MM/dd/yyyy HH:mm"));
+            Console.WriteLine(dateTime.ToString("MM/dd/yyyy hh:mm tt"));
+            Console.WriteLine(dateTime.ToString("MM/dd/yyyy H:mm"));
+            Console.WriteLine(dateTime.ToString("MM/dd/yyyy h:mm tt"));
+            Console.WriteLine(dateTime.ToString("MM/dd/yyyy HH:mm:ss"));
+            Console.WriteLine(dateTime.ToString("MMMM dd"));
+            Console.WriteLine(dateTime.ToString("yyyy’-‘MM’-‘dd’T’HH’:’mm’:’ss.fffffffK"));
+            Console.WriteLine(dateTime.ToString("ddd, dd MMM yyy HH’:’mm’:’ss ‘GMT’"));
+            Console.WriteLine(dateTime.ToString("yyyy’-‘MM’-‘dd’T’HH’:’mm’:’ss"));
+            Console.WriteLine(dateTime.ToString("HH:mm"));
+            Console.WriteLine(dateTime.ToString("hh:mm tt"));
+            Console.WriteLine(dateTime.ToString("H:mm"));
+            Console.WriteLine(dateTime.ToString("h:mm tt"));
+            Console.WriteLine(dateTime.ToString("HH:mm:ss"));
+            Console.WriteLine(dateTime.ToString("yyyy MMMM"));
+
+            Console.ReadKey();
+        }
+
+    }
+}
